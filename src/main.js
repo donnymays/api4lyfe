1b47bcb390ecb3c52be40fd862227fff6213ead3
donnymays <dtmays2@gmail.com>
2020-09-09 16:48:18 -0700

    last commit of the day

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
-import $ from 'jquery';
+//import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+
+
+
+
+// function myMap() {
+// var mapProp= {
+//   center:new google.maps.LatLng(51.508742,-0.120850),
+//   zoom:5,
+// };
+// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// }
+
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+
+
+
+/*
+
+export default class google.maps.Map {
+  static getLocation(city) {
+    return new Promise(function(resolve, reject) {
+      let map = 
+    })
+  }
+}
+
+*/
